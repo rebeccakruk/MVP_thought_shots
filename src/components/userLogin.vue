c636ead1091917f15ceb8e31d685205a3ff850f8
rk <rebeccakruk@outlook.com>
2023-03-06 20:46:41 -0700

    adding vuetify, cleaned up home and about views

---
+++ src/components/userLogin.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import cookies from 'vue-cookies'
+    export default {
+        name: "userLogin"
+    }
+</script>
+
+<style scoped>
+
+</style>
